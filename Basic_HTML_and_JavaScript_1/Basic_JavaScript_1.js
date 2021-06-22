c8ad806709d8769df365eb7865745fcab5403929
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-06-22 02:57:08 -0600

    java script
    made a button
    made two js files for the scripts
    removed scripts from htmls and sourced their js files instead

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert("Hello,World! I AM A VIRUS!!!!! jk... Click the button at your own risk! YOU HAVE BEEN WARNED!!!");
+function My_First_Function(){
+	var String="Kiss me, I'm Irish!";
+	var result=String.fontcolor("green");
+	document.getElementById("Irish").innerHTML=
+	result;}
